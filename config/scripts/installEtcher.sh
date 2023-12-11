1e942187031e73096dc77b4d8b9dc088bf803e5f
pliveyns <pliveyns@users.noreply.github.com>
2023-12-11 18:59:28 +0100

    20231211 Add script to install Etcher

---
+++ config/scripts/installEtcher.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+set -oue pipefail
+
+# Install Balena Etcher AppImage
+echo "Installing Balena Etcher AppImage ---"
+
+package_url="https://github.com/balena-io/etcher/releases/download/v1.18.13/balena-etcher-1.18.13.x86_64.rpm"
+curl -L $package_url -o /tmp/balena-etcher.rpm
+sudo dnf localinstall /tmp/balena-etcher.rpm
+
